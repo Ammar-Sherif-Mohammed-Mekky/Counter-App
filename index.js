431e7210039d8977e526b976691d0c814db2fa3e
Ammar Sherif <ammarmekky2008@gmail.com>
2022-03-22 17:40:30 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+
+count = 0
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function save(){
+    saveEl.textContent += count + " - "
+    countEl.textContent = 0
+    count = 0
+}
